44945f1e5734e6e5459d03ccc0a52d2f5738f272
Tevaalgorithms <45029995+Tevaalgorithms@users.noreply.github.com>
2018-11-16 22:47:37 -0700

    Update and rename Missing_Number.java to MissingNumber.java

---
+++ MissingNumber.java
@@ -0,0 +1,27 @@
+
+public class MissingNumber 
+{
+    public static void missing_number(int[] arr)
+    {
+        int lenght = arr.length;
+        // Total number of n continuous integer from 1 to n is : n * (n+1) /2
+        // NOTE: Here we need to add + 1 to the array lenght since it's missing an elemenet
+        int total = ((lenght + 1) * (lenght + 2)) / 2;
+        
+        for(int i = 0; i < lenght ; i++)
+        {
+            total -= arr[i];
+        }
+        
+        System.out.println(total);
+                
+    }
+    
+    public static void main(String[] args)throws IOException
+    {
+           int[] arr = new int[]{1, 2, 4, 5, 6, 7, 8};
+        
+            // Call maxsum function
+            missing_number(arr);
+    }
+}
