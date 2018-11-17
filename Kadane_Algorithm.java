eef5c6de71c6e93ffd7ac8ebe34a78994ddbe9da
Tevaalgorithms <45029995+Tevaalgorithms@users.noreply.github.com>
2018-11-16 22:42:40 -0700

    Update Kadane_Algorithm.java

--- Kadane_Algorithm.java
+++ Kadane_Algorithm.java
@@ -27,47 +27,12 @@ public class Kadane_Algorithm
         }
         
         System.out.println(global_sum);
-    }
+    } 
     
-    /*
-    Input:
-    The first line of input contains an integer T denoting the number of test cases. 
-    The description of T test cases follows. 
-    The first line of each test case contains a single integer N denoting the size of array. 
-    The second line contains N space-separated integers A1, A2, …, AN denoting the elements of the array.
-    
-    Ouput:
-    Print the maximum sum of the contiguous sub-array in a separate line for each test case.
-    */
-    
-    public static void main(String[] args)throws IOException
-    {
-        BufferedReader input = new BufferedReader( new InputStreamReader(System.in));
-        
-        //Get the total number of test cases
-        String lines = input.readLine();        
-        int total_test = Integer.parseInt(lines);
-        
-        while(total_test > 0)
-        {
-            // Get the array size of each problem 
-            int n = Integer.parseInt(input.readLine());
-            
-            int arr[] = new int[n];
-            
-            // Get the elements of array
-            String lines1 = input.readLine();
-            String[] srs = lines1.trim().split("\\s+");  
-            
-            for(int i = 0; i < srs.length; i++)
-            {
-                arr[i] = Integer.parseInt(srs[i]);
-            }
-            
+    public static void main(String[] args)
+    {      
+           int[] arr = new int{ -1, 10, 5, 67, -80, 45};
             // Call maxsum function
             maxSum(arr);
-            
-            total_test--;
-        }
     }
 }
